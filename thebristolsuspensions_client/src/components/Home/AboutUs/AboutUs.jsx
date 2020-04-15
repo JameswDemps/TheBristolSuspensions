e4550e0b2bfa236e143bd5117b52de413d354b72
James Dempsey <James.Dempsey@nationwide.co.uk>
2020-04-15 19:03:00 +0100

    Fixed Positioning of elements

--- thebristolsuspensions_client/src/components/Home/AboutUs/AboutUs.jsx
+++ thebristolsuspensions_client/src/components/Home/AboutUs/AboutUs.jsx
@@ -9,18 +9,22 @@ export const AboutUs = () => {
     return (
         <>
             <div className='about-us-banner-image'>
-                <div className='about-us-text-container'>
-                    <div className='about-us-text'>
-                        <h1>
-                            {strings.ABOUT_US_TITLE}
-                        </h1>
-                        <p className="about-us-paragraph">
-                            {strings.ABOUT_US_DESCRIPTION}
-                        </p>
+                <div className='about-us-container'>
+                    <div className='about-us-text-container'>
+                        <div className='about-us-text'>
+                            <h1 className='about-us-text-header'>
+                                {strings.ABOUT_US_TITLE}
+                            </h1>
+                            <p className="about-us-paragraph">
+                                {strings.ABOUT_US_DESCRIPTION}
+                            </p>
+                        </div>
                     </div>
-                    <button className='about-us-latest-video'>
-                        Watch our latest live video!
-                    </button>
+                    <div className='about-us-button-container'>
+                        <button className='about-us-latest-video'>
+                            Watch our latest live video!
+                        </button>
+                    </div>
                 </div>
             </div>
         </>
