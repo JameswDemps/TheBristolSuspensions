941bd0bad718ac244899269059955a6f67a4f591
James Dempsey <James.Dempsey@nationwide.co.uk>
2020-05-03 18:17:33 +0100

    Started NavBar

---
+++ thebristolsuspensions_client/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+import "./css/NavBar.css";
+
+export const NavBar = () => {
+  return (
+    <>
+      <div className="NavBar">
+        <div className="logo-homelink"></div>
+        <div className="nav-link">HOME</div>
+        <div className="nav-link">ABOUT</div>
+        <div className="nav-link">MUSIC</div>
+        <div className="nav-link">VIDEOS</div>
+        <div className="nav-link">UPCOMING EVENTS</div>
+        <div className="nav-link">FRINGE</div>
+        <div className="nav-link">CONTACT</div>
+      </div>
+      <div className="Navspace" />
+    </>
+  );
+};
+
+// export default NavBar;
