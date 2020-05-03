3f06591bd7d0401ea30aae22409512f5f5d069be
James Dempsey <James.Dempsey@nationwide.co.uk>
2020-05-03 14:38:19 +0100

    more spotify updates

--- thebristolsuspensions_client/src/components/Home/SpotifySection/SpotifySection.jsx
+++ thebristolsuspensions_client/src/components/Home/SpotifySection/SpotifySection.jsx
@@ -2,27 +2,31 @@ import React from "react";
 
 import Image from "react-image-webp";
 
-import "./css/SpotifySection.css";
+import "./css/spotify-section.css";
 import { Spotify } from "../../Spotify";
 
 export const SpotifySection = () => {
   return (
-    <div className="Spotify-Section-Container">
-      <Spotify />
-      <iframe
-        src="https://open.spotify.com/follow/1/?uri=spotify:artist:29J0P5akUKvNuV3KsPDmLZ&size=detail&theme=light"
-        width="240"
-        height="56"
-        scrolling="no"
-        frameborder="0"
-        // style={border="none"; overflow="hidden";}
-        allowtransparency="true"
-      ></iframe>
-      <Image
-        // className="logo"
-        // src={require('./Images/homepage-banner.png')}
-        webp={require("../../Images/Testament-Final.webp")}
-      />
+    <div className="spotify-section-container">
+      <div className="spotify-section-container-contents">
+        <Spotify />
+        <iframe
+          src="https://open.spotify.com/follow/1/?uri=spotify:artist:29J0P5akUKvNuV3KsPDmLZ&size=detail&theme=light"
+          width="240"
+          height="56"
+          scrolling="no"
+          frameborder="0"
+          style={{ border: "none", overflow: "hidden" }}
+          allowtransparency="true"
+        ></iframe>
+        <Image
+          // className="logo"
+          // src={require('./Images/homepage-banner.png')}
+          webp={require("../../Images/Testament-Final.webp")}
+          // style={{ width: "33%", height: "33%" }}
+          width="33%"
+        />
+      </div>
     </div>
   );
 };
