3423406ddcb6d973e689157d01a9147735eef49b
James Dempsey <James.Dempsey@nationwide.co.uk>
2020-05-20 16:57:56 +0100

    Reached finishing point for the footer

--- thebristolsuspensions_client/src/components/Footer/Footer.jsx
+++ thebristolsuspensions_client/src/components/Footer/Footer.jsx
@@ -6,13 +6,60 @@ export const Footer = () => {
   return (
     <div className="footer-container">
       <div className="footer-heading-text">Follow Us</div>
-      <div className="footer-icons">
-        <div>Facebook</div>
-        <div>Twitter</div>
-        <div>Instagram</div>
-        <div>SoundCloud</div>
-        <div>SnapChat</div>
-        <div>YouTube</div>
+      <div className="footer-icons-container">
+        <div className="footer-icons">
+          <a
+            href="https://www.facebook.com/thebristolsuspensions/"
+            className="footer-icon"
+          >
+            <img
+              className="footer-icon-img"
+              src={require("../Images/Facebook-Footer-Icon.webp")}
+            ></img>
+          </a>
+          <a href="https://twitter.com/brisuspensions" className="footer-icon">
+            <img
+              className="footer-icon-img"
+              src={require("../Images/Twitter-Footer-Icon.webp")}
+            ></img>
+          </a>
+          <a
+            href="https://www.instagram.com/brisuspensions/"
+            className="footer-icon"
+          >
+            <img
+              className="footer-icon-img"
+              src={require("../Images/Instagram-Footer-Icon.webp")}
+            ></img>
+          </a>
+          <a
+            href="https://soundcloud.com/user-914962576"
+            className="footer-icon"
+          >
+            <img
+              className="footer-icon-img"
+              src={require("../Images/SoundCloud-Footer-Icon.webp")}
+            ></img>
+          </a>
+          <a
+            href="https://www.snapchat.com/add/brisuspensions"
+            className="footer-icon"
+          >
+            <img
+              className="footer-icon-img"
+              src={require("../Images/SnapChat-Footer-Icon.webp")}
+            ></img>
+          </a>
+          <a
+            href="https://www.youtube.com/channel/UC64I7Ne7m-ddyCvNxcdfwKw/"
+            className="footer-icon"
+          >
+            <img
+              className="footer-icon-img"
+              src={require("../Images/YouTube-Footer-Icon.webp")}
+            ></img>
+          </a>
+        </div>
       </div>
       <div className="footer-trademark">© 2017 The Bristol Suspensions</div>
     </div>
