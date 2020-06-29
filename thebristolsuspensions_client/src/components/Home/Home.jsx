e217d943fe0ad92622448f10af4390cb4846bde4
James Dempsey <James.Dempsey@nationwide.co.uk>
2020-06-29 11:44:24 +0100

    Fixed Carousel

--- thebristolsuspensions_client/src/components/Home/Home.jsx
+++ thebristolsuspensions_client/src/components/Home/Home.jsx
@@ -17,16 +17,6 @@ export const Home = () => {
       <SocialMedia />
       <CCarousel />
       <SpotifySection />
-      {/*<picture>*/}
-      {/*    <source srcSet={require("../Images/homepage-about-us.webp")} type="image/webp"/>*/}
-      {/*    <source srcSet={require("../Images/homepage-about-us.jpg")} type="image/jpeg"/>*/}
-      {/*    <img src={require("../Images/homepage-about-us.webp")} alt="Alt Text!" />*/}
-      {/*</picture>*/}
-      {/*<Image*/}
-      {/*    className='about-us-banner-image'*/}
-      {/*    src={require('../Images/homepage-about-us.jpg')}*/}
-      {/*    webp={require('../Images/homepage-about-us.webp')}*/}
-      {/*/>*/}
     </>
   );
 };
--- thebristolsuspensions_client/src/components/NavBar/NavBar.jsx
+++ thebristolsuspensions_client/src/components/NavBar/NavBar.jsx
@@ -19,5 +19,3 @@ export const NavBar = () => {
     </>
   );
 };
-
-// export default NavBar;
