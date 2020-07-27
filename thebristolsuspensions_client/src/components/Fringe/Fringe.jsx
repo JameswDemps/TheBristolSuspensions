5f31329197ece4fcadd27b9230f559f0d0521fdd
James Dempsey <James.Dempsey@nationwide.co.uk>
2020-07-27 10:55:08 +0100

    add image to fringe

--- thebristolsuspensions_client/src/components/Fringe/Fringe.jsx
+++ thebristolsuspensions_client/src/components/Fringe/Fringe.jsx
@@ -1,5 +1,26 @@
 import React from "react";
 
+import "./css/fringe.css";
+import Image from "react-image-webp";
+
 export const Fringe = () => {
-  return <>Hello!</>;
+  return (
+    <div className="fringe">
+      <div className="fringe-content">
+        <div className="fringe-text">
+          <h1>
+            The Bristol Suspensions are returning to The Fringe for a fifth
+            consecutive year!
+          </h1>
+        </div>
+        <div className="fringe-poster">
+          <Image
+            className="fringe-poster-image"
+            // src={require('./Images/homepage-banner.png')}
+            webp={require("../Images/poster.webp")}
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
