6c6bfd90c855c0729e8f2e22ae173e361656cf40
James Dempsey <James.Dempsey@nationwide.co.uk>
2020-08-05 16:04:27 +0100

    Finished MVP About Us page

--- thebristolsuspensions_client/src/components/About/AboutUs/AboutUs.jsx
+++ thebristolsuspensions_client/src/components/About/AboutUs/AboutUs.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import "./css/about-us.css";
+import "../../../App.css";
 import Image from "react-image-webp";
 
 import { strings } from "./strings";
@@ -8,7 +9,7 @@ import { strings } from "./strings";
 export const AboutUs = () => {
   return (
     <div className="about-us">
-      <div className="about-us-banner">
+      <div className="about-us-banner about-us-banner-heading-background">
         <div className="about-us-banner-overlay">
           <h1 className="about-us-header">{strings.ABOUT_US_HEADING}</h1>
         </div>
@@ -18,6 +19,40 @@ export const AboutUs = () => {
           {strings.ABOUT_US_TEXT.map((item) => (
             <p>{item}</p>
           ))}
+        </div>
+      </div>
+      <div className="about-us-banner about-us-banner-awards-background">
+        <div className="about-us-banner-overlay">
+          <h1 className="about-us-header">{strings.ABOUT_US_AWARDS_HEADING}</h1>
+        </div>
+      </div>
+      <div className="about-us-text-container">
+        <div className="about-us-text">
+          {strings.ABOUT_US_AWARDS.map((item) => (
+            <p>
+              <span>{`${item.EMOJI} `}</span>
+              <strong>{item.NAME}</strong>
+              <span>{` - ${item.CONTEXT}`}</span>
+            </p>
+          ))}
+        </div>
+      </div>
+      <div className="wrapper">
+        <div className="snow-wrapper">
+          <div className="content">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. .... ....
+              Donec in justo eu ligula semper consequat sed a risus.
+            </p>
+          </div>
+        </div>
+        <div className="content-wrapper">
+          <div className="content">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. .... ....
+              Donec in justo eu ligula semper consequat sed a risus.
+            </p>
+          </div>
         </div>
       </div>
     </div>
--- thebristolsuspensions_client/src/components/About/AboutUs/strings.js
+++ thebristolsuspensions_client/src/components/About/AboutUs/strings.js
@@ -5,4 +5,107 @@ export const strings = {
     "Formed in October 2014, the group has cemented its status as an established group in the UK A Cappella scene. Now in their fifth year, they have already recorded two albums, released three music videos and many live videos, been on the radio four times, placed at the ICCA Semifinals, had two official sell out Fringe runs, toured North-Eastern USA, received awards for Outstanding Arrangement, Choreography and Vocal Percussion, appeared on BBC One's Pitch Battle and won The Voice Festival UK.",
     "We perform regularly at both private and public functions. Check out our videos page to see more from us, and head to our contact page if you want to find out more or enquire about booking us for an event.",
   ],
+  ABOUT_US_AWARDS_HEADING: "Awards",
+  ABOUT_US_AWARDS: [
+    {
+      EMOJI: "🏆",
+      NAME: "Second Place",
+      CONTEXT: "ICCA UK Final 2019",
+    },
+    {
+      EMOJI: "⭐",
+      NAME: "Outstanding Choregraphy",
+      CONTEXT: "ICCA UK Final 2019",
+    },
+    {
+      EMOJI: "⭐",
+      NAME: "Outstanding Vocal Percussion",
+      CONTEXT: "ICCA UK Final 2019",
+    },
+    {
+      EMOJI: "🏆",
+      NAME: "Quarter-Final Champion",
+      CONTEXT: "ICCA Birmingham Quarter-Final 2019",
+    },
+    {
+      EMOJI: "🏆",
+      NAME: "Third Place",
+      CONTEXT: "ICCA International Wildcard 2018",
+    },
+    {
+      EMOJI: "🏆",
+      NAME: "Second Place",
+      CONTEXT: "ICCA UK Final 2018",
+    },
+    {
+      EMOJI: "⭐",
+      NAME: "Outstanding Choregraphy",
+      CONTEXT: "ICCA UK Final 2018",
+    },
+    {
+      EMOJI: "⭐",
+      NAME: "Outstanding Vocal Percussion",
+      CONTEXT: "ICCA UK Final 2018",
+    },
+    {
+      EMOJI: "🏆",
+      NAME: "Quarter-Final Champion",
+      CONTEXT: "ICCA South West Quarter-Final 2018",
+    },
+    {
+      EMOJI: "⭐",
+      NAME: "Outstanding Arrangement",
+      CONTEXT: "ICCA South West Quarter-Final 2018",
+    },
+    {
+      EMOJI: "⭐",
+      NAME: "Outstanding Vocal Percussion",
+      CONTEXT: "ICCA South West Quarter-Final 2018",
+    },
+    {
+      EMOJI: "🏆",
+      NAME: "Third Place",
+      CONTEXT: "ICCA London Semi-Final 2017",
+    },
+    {
+      EMOJI: "🏆",
+      NAME: "Quarter-Final Champion",
+      CONTEXT: "ICCA South West Quarter-Final 2017",
+    },
+    {
+      EMOJI: "⭐",
+      NAME: "Outstanding Choreography",
+      CONTEXT: "ICCA South West Quarter-Final 2017",
+    },
+    {
+      EMOJI: "🌟",
+      NAME: "Official Sell Out Fringe Run",
+      CONTEXT: "Edinburgh Fringe Festival 201",
+    },
+    {
+      EMOJI: "🏆",
+      NAME: "University Champions",
+      CONTEXT: "VFUK 2016",
+    },
+    {
+      EMOJI: "⭐",
+      NAME: "Outstanding Beatboxing",
+      CONTEXT: "VFUK 2016",
+    },
+    {
+      EMOJI: "⭐",
+      NAME: "Outstanding Choreography",
+      CONTEXT: "VFUK 2016",
+    },
+    {
+      EMOJI: "⭐",
+      NAME: "Outstanding Vocal Percussion",
+      CONTEXT: "ICCA UK Semi-Final 2016",
+    },
+    {
+      EMOJI: "⭐",
+      NAME: "Outstanding Arrangement",
+      CONTEXT: "ICCA South West Quarter-Final 2016",
+    },
+  ],
 };
