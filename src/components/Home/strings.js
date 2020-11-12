b1f1989d963617c2c3334f3e4082b3e36a368375
James Dempsey <James.Dempsey@nationwide.co.uk>
2020-11-12 13:37:12 +0000

    Azure test 13

--- src/components/Home/strings.js
+++ src/components/Home/strings.js
@@ -1,5 +1,5 @@
 export const strings = {
-  ABOUT_US_TITLE: "ABOUT US12",
+  ABOUT_US_TITLE: "ABOUT US13",
   ABOUT_US_DESCRIPTION:
     "The Bristol Suspensions are an award winning mixed a cappella group from the University of Bristol, and are Champions of UK A Cappella, after ​winning The Voice Festival UK in April 2016. The group are renowned for their electric performance, slick choreography and stunning vocals.",
 };
